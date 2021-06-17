1844347bdbacd9c081978704855cad31bbc159a8
VilmaIkonen <vilma.r.ikonen@gmail.com>
2021-06-17 10:58:34 +0300

    Sorting for all columns

--- src/pages/VaccineOrdersPage/VaccineOrdersAllTable.js
+++ src/pages/VaccineOrdersPage/VaccineOrdersAllTable.js
@@ -1,15 +1,14 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { useFilters, useTable } from 'react-table'
+import { useFilters, useSortBy, useTable } from 'react-table'
 
 export function VaccineOrdersAllTable({ data, columns }) {
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, setFilter } // filter is set via useFilter hook 
-    = useTable({ data, columns }, useFilters) // useFilter hook for passing filter value to table and for filtering the table rows
-
   const [filterInput, setFilterInput] = useState('')
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, setFilter } // filter is set via useFilter hook 
+    = useTable({ data, columns }, useFilters, useSortBy) // useFilter & useSortBy hook fors passing filter value to table and for filtering/sorting the table rows/columns
 
-  // Update filter state when change in input/keyword occurs
+  // Search responsible person. Update filter state when change in input/keyword occurs
   const handleFilterChange = (event) => {
     let keyword = event.target.value || undefined
     setFilter('responsiblePerson', keyword) // update responsiblePerson filter
@@ -20,7 +19,7 @@ export function VaccineOrdersAllTable({ data, columns }) {
     <div>
       <Link to='/'><button>Back home</button></Link>
       <Link to='/vaccinations'><button>Vaccinations page</button></Link>
-      <input value={filterInput} onChange={handleFilterChange} placeholder={'Search name'}></input>
+      <input value={filterInput} onChange={handleFilterChange} placeholder={'Search responsible person'}></input>
       <div>
         <table {...getTableProps()}>
           <thead>       
@@ -29,7 +28,15 @@ export function VaccineOrdersAllTable({ data, columns }) {
                 <tr {...headerGroup.getHeaderGroupProps()}>
                   {headerGroup.headers.map(column => {
                     return(
-                      <th {...column.getHeaderProps()}>
+                      <th {...column.getHeaderProps(column.getSortByToggleProps())} // will allow sorting of all columns (use disableSortBy per column if needed)
+                      // Based on sorting, add classNames
+                      className={
+                        column.isSorted 
+                        ? column.isSortedDesc
+                          ? 'sort-descending'
+                          : 'sort-ascending'
+                        : ''
+                      }>
                         {column.render('Header')}
                       </th> 
                     )                  
