636f133dde6973d0e845d00236329091750e2e80
VilmaIkonen <vilma.r.ikonen@gmail.com>
2021-06-17 13:12:04 +0300

    Pagination to vaccine orders

--- src/pages/VaccineOrdersPage/VaccineOrdersAllTable.js
+++ src/pages/VaccineOrdersPage/VaccineOrdersAllTable.js
@@ -1,12 +1,35 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { useFilters, useSortBy, useTable } from 'react-table'
+import { useFilters, usePagination, useSortBy, useTable } from 'react-table'
 
 export function VaccineOrdersAllTable({ data, columns }) {
 
   const [filterInput, setFilterInput] = useState('')
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, setFilter } // filter is set via useFilter hook 
-    = useTable({ data, columns }, useFilters, useSortBy) // useFilter & useSortBy hook fors passing filter value to table and for filtering/sorting the table rows/columns
+  const { 
+      getTableProps, 
+      getTableBodyProps, 
+      headerGroups, 
+      prepareRow,
+      /*rows*/ page, 
+      canPreviousPage,
+      canNextPage,
+      pageOptions,
+      pageCount,
+      gotoPage,
+      nextPage,
+      previousPage,
+      setPageSize,
+      state: { pageIndex, pageSize },
+      setFilter } // filter is set via useFilter hook. For pagination 'page' is used instead of rows
+    = useTable({ 
+        data, 
+        columns,
+        initialState: { } // defaults to pageIndex and pageSize are 0 and 10 respectively
+      }, 
+        useFilters, 
+        useSortBy,
+        usePagination
+      ) // useFilter & useSortBy hook fors passing filter value to table and for filtering/sorting the table rows/columns
 
   // Search responsible person. Update filter state when change in input/keyword occurs
   const handleFilterChange = (event) => {
@@ -46,7 +69,7 @@ export function VaccineOrdersAllTable({ data, columns }) {
             })}                       
           </thead>
           <tbody {...getTableBodyProps()}>
-            {rows.map((row) => {
+            {page.map((row) => {
               prepareRow(row)
               return (
                 <tr {...row.getRowProps()}>
@@ -58,6 +81,37 @@ export function VaccineOrdersAllTable({ data, columns }) {
             })}
           </tbody>
         </table>
+        <div className='pagination'>
+          <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>{'<<'}</button>
+          <button onClick={() => previousPage()} disabled={!canPreviousPage}>{'<'}</button>
+          <button onClick={() => nextPage()} disabled={!canNextPage}>{'>'}</button>
+          <button onClick={() => gotoPage(pageCount -1)} disabled={!canNextPage}>{'>>'}</button>
+          <span>Page{' '}
+            <strong>{pageIndex +1} of {pageOptions.length}</strong>
+          </span>
+          {/* Moving to user selected page no.: */}
+          <span>{' '}Go to page:{' '}
+            <input 
+              type='number'
+              defaultValue={pageIndex +1}
+              onChange={event => {
+                let page = event.target.value ? Number(event.target.value) -1 : 0
+                gotoPage(page)
+              }}
+            />
+          </span>
+          {/* User selection of rows to show/page */}
+          <select
+            value={pageSize}
+            onChange={event => {
+              setPageSize(Number(event.target.value))
+            }}  
+          >
+            {[10, 20, 50, 100].map(pageSize => {
+              return <option key={pageSize} value={pageSize}>Show {pageSize}</option>       
+            })}            
+          </select>
+        </div>
       </div>
     </div>
   )
